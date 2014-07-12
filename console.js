41968becd45ee28b78d343dbee28af5d730e2246
JAForbes <james.a.forbes@gmail.com>
2014-07-13 01:24:22 +1000

    Forced evals expressions allow fns to be eval'd

--- console.js
+++ console.js
@@ -4,24 +4,28 @@
 
   //override console.log
   console.log = function(result){
-    $output.prepend('<code>'+result+'</code>');
+    $output.prepend('<code>'+eval(expression(result))+'</code>');
   }
 
   //proxy input to console.log
   $('textarea').keydown(function(e){
     var enterCode = 13;
     if(e.keyCode == enterCode){
-      console.log(eval($(this).val()))
+      console.log($(this).val())
       $(this).val('')
     }
   })
 
 
+  //convert a js string into an expression
+  function expression(str){
+    return '(' + str + ')'
+  }
+
   //listen for errors and display them
   window.onerror = function(error, url, line) {
     console.log(error);
   };
-
 
 
 })()
